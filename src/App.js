1d7d444977b7a4a578b86331b26b656495e17bc2
akmalahmadov <92873076+akmalahmadov@users.noreply.github.com>
2021-12-20 19:46:27 +0500

    hdg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Murojat from './Components/Murojat';
+import Page from './Components/Page';
+
 
 function App() {
   return (
     <>
     <Murojat />
+    <Page/>
     </>
   );
 }
