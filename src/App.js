6f2191582f09e78aa7719d78df4e5c7395c12084
Xursanali <bxursanali@gmail.com>
2021-12-22 19:30:04 +0500

    Salom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from "react-router-dom";
 import Murojat from "./Components/Murojat";
 import Working from "./Components/Working";
-import Hoverpost from "./Components/Hoverpost";
-import Page from './Components/Page';
+import Header from "./Components/Header";
 
 function App() {
     return (
-    <BrowserRouter>
-      <Header />
-      <Route path='/murojat' component={Murojat} />
-      <Route path='/working' component={Working} />
-      <Footer />
-    </BrowserRouter>
+        <BrowserRouter>
+            <Header />
+            <Route path="/murojat" component={Murojat} />
+            <Route path="/working" component={Working} />
+        </BrowserRouter>
     );
-
 }
 
 export default App;
