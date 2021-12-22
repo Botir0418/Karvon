b4dab98d3a514336181a4dcfa92f9654ef7ec1ed
Botir0418 <karimovbotir55@gmail.com>
2021-12-22 18:19:11 +0500

    asdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Murojat from './Components/Murojat';
+import Working from './Components/Working';
 
 function App() {
   return (
     <>
-    <Murojat />
+        {/* <Murojat />
+        <Working /> */}
     </>
   );
 }
